3aa208e6734eb7ce83582d86409423a442477a22
Rbttt <golomazdinp@mail.ru>
2022-04-02 12:20:17 +0300

    Bsically the front-end is done. Some foregrounds and features can be added. It also needs to be made responsive.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const controlButtons = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.section');
 const mainContent = document.querySelector('.main-content');
 const foregroundElements = document.querySelectorAll('.foreground-element');
+const dynamicBackgrounds= document.querySelectorAll('.dynamic-shape');
+const themeToggleButton = document.querySelector('.theme-btn');
+console.log(dynamicBackgrounds);
 
 function controlButtonSwitching(){
     for(let i=0; i < controlButtons.length; i++){
@@ -24,6 +27,7 @@ function controlActiveElements(){
         if(id){
             switchSection(id);
             switchForeGround(id);
+            switchBackground(id);
         }
     })
 }
@@ -45,9 +49,25 @@ function switchForeGround(elementId){
     })
 }
 
+function switchBackground(elementId){
+    dynamicBackgrounds.forEach((element) =>{
+        element.classList.remove('active-background');
+        if(element.classList[element.classList.length-1] == elementId){
+            element.classList.add('active-background');
+        }
+    })
+}
+
+function controlThemeToggling(){
+    themeToggleButton.addEventListener('click', () =>{
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    });
+}
 
 controlButtonSwitching();
 controlActiveElements();
+controlThemeToggling();
 
 // console.log(document.getElementsByClassName("anime-list-link")[0].get("class"));
 
